9f74f48639d7dc4e79188781d1c2fec191f3e564
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2024-09-16 15:04:52 -0300

    update inss calculation

--- server.py
+++ server.py
@@ -19,14 +19,22 @@ def calcular_pagamento(salario_mensal, horas_trabalhadas, horas_extras, mes_vige
     dsr = (valor_horas_extras / 6)
     
     # Cálculo da contribuição ao INSS
+        # primeira faixa do inss - 1412*0.075 = 105.9
+        # segunda faixa do inss - (2666.68 - 1412) *0.09 = 112.92
+        # terceira faixa do inss - (4000.03 - 2666.68) * 0.12 = 160
+        # quarta faixa do inss - (7786.02 - 4000.03 ) * 0.14 = 530.03
+        
     if salario_mensal <= 1212.00:
         inss = salario_mensal * 0.075
-    elif salario_mensal <= 2427.35:
-        inss = salario_mensal * 0.09
-    elif salario_mensal <= 3641.03:
-        inss = salario_mensal * 0.12
+    elif salario_mensal <= 2666.68:
+        inss = ((salario_mensal - 1412) * 0.09) + 105.9 
+    elif salario_mensal <= 4000.03:
+        inss = ((salario_mensal - 2666.68) * 0.12) + 105.9 + 112.92
+    elif salario_mensal <= 7786.02:
+        inss = ((salario_mensal - 4000.03) * 0.14) + 105.9 + 112.92 + 160.00
     else:
-        inss = salario_mensal * 0.14
+        #teto de contribuicao do inss
+        inss = 105.9 + 112.92 + 160 + 530.03
     
     # Valor total a ser pago
     valor_total = salario_mensal + valor_horas_extras + dsr - inss
